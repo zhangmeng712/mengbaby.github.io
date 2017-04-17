14a23c0654fc54243111c5c9983d0ad4322f1f21
sifei.zm <sifei.zm@taobao.com>
2017-04-17 23:59:38 +0800

    “update”

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-hexo clean
-hexo generate
-cp -R public/* .deploy/mengbaby.github.io
-cd .deploy/mengbaby.github.io
-git add .
-git commit -m “update”
-git push origin master
--- source/tags/index.md
+++
@@ -1,6 +0,0 @@
----
-title: 标签云
-date: 2017-04-17 01:33:49
-layout: tags
-comments: false
----
